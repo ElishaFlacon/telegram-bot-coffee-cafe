0ba5e76661722e55aff78431d5b91a9ca0ff23fe
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2022-09-27 21:10:36 +0500

    da

--- session/worker_session.py
+++ session/worker_session.py
@@ -63,6 +63,7 @@ def append_end_session_on_data(worker_name):
     except Exception as e:
         print(f'worker_session Строка №59 - {e}')
 
+
 # В теории почти все функции выше, в этом файле, можно объеденить, но я писал их в самом начале
 # А сейчас уже 36 день, как я делаю бота и мне лень это переписывать (:
 
@@ -76,3 +77,13 @@ def check_worker_session(worker_name, date):
                 print('ДОДЕЛАТЬ ФУНКЦИОНАЛ')
     except Exception as e:
         print(f'worker_session Строка №59 - {e}')
+
+
+# Просмотр активных смен сотрудников
+def check_active_sessions():
+    rtn_str = ''
+    max_rw_workers = WORKERS_SHEET.max_row
+    for i in range(max_rw_workers):
+        if str(WORKERS_SHEET[f'C{i+1}'].value) == 'Да':
+            rtn_str += f'{WORKERS_SHEET[f"B{i+1}"].value}\n'
+    return rtn_str
--- verification/admin_verify.py
+++ verification/admin_verify.py
@@ -11,12 +11,23 @@ def admin_verify(admin_id):
         print(f'admin_verify Строка №11 - {e}')
 
 
-# Добавление новоо сотрудника, почему эта функция здесь, спросите
+# Добавление-удаление сотрудника, почему эта функция здесь, спросите
 # А воооот, потому что могу
 # А вообще стоило разбить почти пол проекта на еще более меньшие компоненты
 # Учту это на будующее
 # Но а сейчас я 'устану' это делать
 # Я не думал что бот выйдет на 1000+ строк
 # Так, прикинул, ну там на 400 - 600...
-def create_new_worker():
-    pass
+def change_workers(worker_name, worker_id, option):
+    max_rw_workers = WORKERS_SHEET.max_row
+    for i in range(max_rw_workers + 1):
+        if WORKERS_SHEET[f'B{i+1}'].value == None:
+            if option == True:
+                WORKERS_SHEET[f'B{i+1}'] = str(worker_name)
+                WORKERS_SHEET[f'C{i+1}'] = str(worker_id)
+                WORKERS_SHEET[f'D{i+1}'] = 'Нет'
+            elif option == False:
+                WORKERS_SHEET[f'B{i+1}'] = ''
+                WORKERS_SHEET[f'C{i+1}'] = ''
+                WORKERS_SHEET[f'D{i+1}'] = ''
+    save_data()
